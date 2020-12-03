0819da1a5dcda740ea58fb1f5f427df98908c9a4
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:06:44 +0300

    2020:12:04 00:06

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-dock',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/.DS_Store','scripts/dock','scripts/dock-add-app','scripts/dock-add-folder','scripts/dock-apps','scripts/dock-folders','scripts/dock-rm']
